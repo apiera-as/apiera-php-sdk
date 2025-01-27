a39fa2594854410c20ba0333e958afa627615364
fredrik-tveraaen <fredrik.tveraaen@apiera.io>
2025-01-27 21:42:12 +0100

    Update ApieraSdk to pass apiera cs

--- src/ApieraSdk.php
+++ src/ApieraSdk.php
@@ -7,14 +7,12 @@ namespace Apiera\Sdk;
 use Apiera\Sdk\DataMapper\AlternateIdentifierDataMapper;
 use Apiera\Sdk\DataMapper\AttributeDataMapper;
 use Apiera\Sdk\DataMapper\CategoryDataMapper;
-use Apiera\Sdk\Exception\ClientException;
 use Apiera\Sdk\Resource\AlternateIdentifierResource;
 use Apiera\Sdk\Resource\AttributeResource;
 use Apiera\Sdk\Resource\CategoryResource;
 
 /**
  * @author Fredrik Tveraaen <fredrik.tveraaen@apiera.io>
- * @package Apiera\Sdk
  * @since 0.1.0
  */
 final readonly class ApieraSdk
@@ -22,8 +20,7 @@ final readonly class ApieraSdk
     private Client $client;
 
     /**
-     * @param Configuration $configuration
-     * @throws ClientException
+     * @throws \Apiera\Sdk\Interface\ClientExceptionInterface
      */
     public function __construct(
         private Configuration $configuration,
@@ -31,30 +28,24 @@ final readonly class ApieraSdk
         $this->client = new Client($this->configuration);
     }
 
-    /**
-     * @return CategoryResource
-     */
     public function category(): CategoryResource
     {
         $dataMapper = new CategoryDataMapper();
+
         return new CategoryResource($this->client, $dataMapper);
     }
 
-    /**
-     * @return AttributeResource
-     */
     public function attribute(): AttributeResource
     {
         $dataMapper = new AttributeDataMapper();
+
         return new AttributeResource($this->client, $dataMapper);
     }
 
-    /**
-     * @return AlternateIdentifierResource
-     */
     public function alternateIdentifier(): AlternateIdentifierResource
     {
         $dataMapper = new AlternateIdentifierDataMapper();
+
         return new AlternateIdentifierResource($this->client, $dataMapper);
     }
 }
