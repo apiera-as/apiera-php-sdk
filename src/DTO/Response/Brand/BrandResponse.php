88711cdd622aa714a50d9016e952197977127f0f
marierinden <marie@shoppingnorge.no>
2025-01-29 12:59:33 +0100

    Add BrandResponse DTO class
    Introduce the `BrandResponse` DTO to encapsulate brand-related response data. This class extends `AbstractResponse` and includes properties for `name` and `description` with corresponding getters.

---
+++ src/DTO/Response/Brand/BrandResponse.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Apiera\Sdk\DTO\Response\Brand;
+
+use Apiera\Sdk\DTO\Response\AbstractResponse;
+use Apiera\Sdk\Enum\LdType;
+use DateTimeInterface;
+use Symfony\Component\Uid\Uuid;
+
+/**
+ * @author Marie Rinden <marie@shoppingnorge.no>
+ * @since 0.3.0
+ */
+final readonly class BrandResponse extends AbstractResponse
+{
+    public function __construct(
+        string $id,
+        LdType $type,
+        Uuid $uuid,
+        DateTimeInterface $createdAt,
+        DateTimeInterface $updatedAt,
+        private string $name,
+        private string $description
+    ) {
+        parent::__construct(
+            $id,
+            $type,
+            $uuid,
+            $createdAt,
+            $updatedAt
+        );
+    }
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+}
