2352a8ae75f8232b37343f043b5f0096851cf19f
marierinden <marie@shoppingnorge.no>
2025-01-29 13:07:21 +0100

    Update return type in BrandCollectionResponse::getMembers
    Replaced the return type annotation from ResponseInterface to BrandResponse for better type clarity and accuracy. Removed an unused import statement to clean up the code.

--- src/DTO/Response/Brand/BrandCollectionResponse.php
+++ src/DTO/Response/Brand/BrandCollectionResponse.php
@@ -5,16 +5,16 @@ declare(strict_types=1);
 namespace Apiera\Sdk\DTO\Response\Brand;
 
 use Apiera\Sdk\DTO\Response\AbstractCollectionResponse;
-use Apiera\Sdk\Interface\DTO\ResponseInterface;
 
 /**
  * * @author Marie Rinden <marie@shoppingnorge.no>
+ *
  * @since 0.3.0
 */
 final readonly class BrandCollectionResponse extends AbstractCollectionResponse
 {
     /**
-     * @return array<ResponseInterface>
+     * @return array<BrandResponse>
      */
     public function getMembers(): array
     {
