047e7ee11f3af341b737d4ee253968b4f6d201f1
fredrik-tveraaen <fredrik.tveraaen@apiera.io>
2025-01-30 13:30:31 +0100

    Add unit tests for InventoryRequest DTO
    Introduce unit tests to validate the behavior of `InventoryRequest`. Tests include instance type verification, constructor and getter methods, minimal parameter handling, and the `toArray` method. These ensure the class works as expected under various scenarios.

---
+++ tests/Unit/DTO/Request/Inventory/InventoryRequestTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\DTO\Request\Inventory;
+
+use Apiera\Sdk\DTO\Request\Inventory\InventoryRequest;
+use Apiera\Sdk\Interface\DTO\RequestInterface;
+use PHPUnit\Framework\TestCase;
+
+final class InventoryRequestTest extends TestCase
+{
+    public function testInstanceOf(): void
+    {
+        $request = new InventoryRequest(0, '');
+
+        $this->assertInstanceOf(InventoryRequest::class, $request);
+        $this->assertInstanceOf(RequestInterface::class, $request);
+    }
+
+    public function testConstructorAndGetters(): void
+    {
+        $request = new InventoryRequest(
+            quantity: 1,
+            sku: '/api/v1/skus/123',
+            inventoryLocation: '/api/v1/inventory_locations/123',
+            iri: '/api/v1/inventory_locations/123/inventories/456'
+        );
+
+        $this->assertEquals(1, $request->getQuantity());
+        $this->assertEquals('/api/v1/skus/123', $request->getSku());
+        $this->assertEquals('/api/v1/inventory_locations/123', $request->getInventoryLocation());
+        $this->assertEquals('/api/v1/inventory_locations/123/inventories/456', $request->getIri());
+    }
+
+    public function testConstructorWithMinimalParameters(): void
+    {
+        $request = new InventoryRequest(quantity: 1, sku: '/api/v1/skus/123');
+
+        $this->assertEquals(1, $request->getQuantity());
+        $this->assertNull($request->getSku());
+        $this->assertNull($request->getInventoryLocation());
+        $this->assertNull($request->getIri());
+    }
+
+    public function testToArray(): void
+    {
+        $request = new InventoryRequest(
+            quantity: 1,
+            sku: '/api/v1/skus/123',
+            inventoryLocation: '/api/v1/inventory_locations/123',
+            iri: '/api/v1/inventory_locations/123/inventories/456'
+        );
+
+        $array = $request->toArray();
+
+        $this->assertIsArray($array);
+        $this->assertArrayHasKey('quantity', $array);
+        $this->assertArrayHasKey('sku', $array);
+        $this->assertArrayNotHasKey('inventory_location', $array);
+        $this->assertArrayNotHasKey('iri', $array);
+
+        $this->assertEquals([
+            'quantity' => 1,
+            'sku' => '/api/v1/skus/123',
+        ], $array);
+    }
+}
