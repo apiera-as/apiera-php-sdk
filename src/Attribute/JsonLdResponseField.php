4924cad5096f5d9acd2a9c90854e9b2ddda23312
fredrik-tveraaen <fredrik.tveraaen@apiera.io>
2025-02-01 21:46:31 +0100

    Add request and response-related attributes
    Introduce attributes `RequestField`, `ResponseField`, `JsonLdResponseField`, and `SkipRequest`. These classes provide structured metadata for handling request and response fields in the SDK, including optional support for transformers. This addition enhances flexibility and simplifies property-level configuration.

---
+++ src/Attribute/JsonLdResponseField.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Apiera\Sdk\Attribute;
+
+use Apiera\Sdk\Interface\ResponseFieldInterface;
+use Attribute;
+
+/**
+ * @author Fredrik Tveraaen <fredrik.tveraaen@apiera.io>
+ * @since 0.3.0
+ */
+#[Attribute(Attribute::TARGET_PROPERTY)]
+final readonly class JsonLdResponseField implements ResponseFieldInterface
+{
+    public function __construct(
+        private string $name,
+        private ?string $transformerClass = null,
+    ) {
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getTransformerClass(): ?string
+    {
+        return $this->transformerClass;
+    }
+}
