f662bd64fd5b7e05bafc7c9a449c635076f853c5
marierinden <marie@shoppingnorge.no>
2025-02-03 12:51:23 +0100

    Add inventory location resource and related DTOs (#34)
    Introduce InventoryLocationResource with CRUD operations and data transformation capabilities. Add supporting request and response DTOs for inventory location handling. Update ApieraSdk to expose the new inventory location resource.

---
+++ src/DTO/Response/InventoryLocation/InventoryLocationCollectionResponse.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Apiera\Sdk\DTO\Response\InventoryLocation;
+
+use Apiera\Sdk\DTO\Response\AbstractCollectionResponse;
+
+/**
+ * @author Fredrik Tveraaen <fredrik.tveraaen@apiera.io>
+ * @since 0.3.0
+ */
+final readonly class InventoryLocationCollectionResponse extends AbstractCollectionResponse
+{
+    /**
+     * @return array<InventoryLocationResponse>
+     */
+    public function getMembers(): array
+    {
+        /** @var array<InventoryLocationResponse> $members */
+        $members = parent::getMembers();
+
+        return $members;
+    }
+}
