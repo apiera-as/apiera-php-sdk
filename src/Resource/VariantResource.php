66bca9972bc598ce689805fee38bb89b590cc27f
marierinden <marie@shoppingnorge.no>
2025-02-04 11:34:15 +0100

    Add VariantResource for variant-related API operations
    This commit introduces the VariantResource class, implementing CRUD and query operations for variants using the API. It ensures validation of requests and leverages data mapping for consistent data handling. These additions enhance the SDK's capabilities for managing variant entities effectively.

---
+++ src/Resource/VariantResource.php
@@ -0,0 +1,175 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Apiera\Sdk\Resource;
+
+use Apiera\Sdk\DTO\QueryParameters;
+use Apiera\Sdk\DTO\Request\Variant\VariantRequest;
+use Apiera\Sdk\DTO\Response\Variant\VariantCollectionResponse;
+use Apiera\Sdk\DTO\Response\Variant\VariantResponse;
+use Apiera\Sdk\Exception\InvalidRequestException;
+use Apiera\Sdk\Interface\ClientInterface;
+use Apiera\Sdk\Interface\DataMapperInterface;
+use Apiera\Sdk\Interface\DTO\RequestInterface;
+use Apiera\Sdk\Interface\RequestResourceInterface;
+
+/**
+ * @author Marie Rinden <marie@shoppingnorge.no>
+ * @since 0.3.0
+ */
+final readonly class VariantResource implements RequestResourceInterface
+{
+    private const string ENDPOINT = '/variants';
+
+    public function __construct(
+        private ClientInterface $client,
+        private DataMapperInterface $mapper,
+    ) {
+    }
+
+    /**
+     * @throws InvalidRequestException
+     * @throws \Apiera\Sdk\Exception\Http\ApiException
+     * @throws \Apiera\Sdk\Exception\Mapping\MappingException
+     */
+    public function find(RequestInterface $request, ?QueryParameters $params = null): VariantCollectionResponse
+    {
+        if (!$request instanceof VariantRequest) {
+            throw new InvalidRequestException(
+                sprintf('Request must be an instance of %s', VariantRequest::class)
+            );
+        }
+
+        if (!$request->getStore()) {
+            throw new InvalidRequestException('Store IRI is required for this operation');
+        }
+
+        /** @var VariantCollectionResponse $collectionResponse */
+        $collectionResponse = $this->mapper->fromCollectionResponse($this->client->decodeResponse(
+            $this->client->get($request->getStore() . self::ENDPOINT, $params)
+        ));
+
+        return $collectionResponse;
+    }
+
+    /**
+     * @throws InvalidRequestException
+     * @throws \Apiera\Sdk\Exception\Http\ApiException
+     * @throws \Apiera\Sdk\Exception\Mapping\MappingException
+     */
+    public function findOneBy(RequestInterface $request, QueryParameters $params): VariantResponse
+    {
+        if (!$request instanceof Variantrequest) {
+            throw new InvalidRequestException(
+                sprintf('Request must be an instance of %s', VariantRequest::class)
+            );
+        }
+
+        $collection = $this->find($request, $params);
+
+        if ($collection->getTotalItems() < 1) {
+            throw new InvalidRequestException('No variant found matching the given criteria');
+        }
+
+        return $collection->getMembers()[0];
+    }
+
+        /**
+         * @throws InvalidRequestException
+         * @throws \Apiera\Sdk\Exception\Http\ApiException
+         * @throws \Apiera\Sdk\Exception\Mapping\MappingException
+         */
+    public function get(RequestInterface $request): VariantResponse
+    {
+        if (!$request instanceof VariantRequest) {
+            throw new InvalidRequestException(
+                sprintf('Request must be an instance of %s', VariantRequest::class)
+            );
+        }
+
+        if (!$request->getIri()) {
+            throw new InvalidRequestException('Variant IRI is required for this operation');
+        }
+
+        /** @var VariantResponse $response */
+        $response = $this->mapper->fromResponse($this->client->decodeResponse(
+            $this->client->get($request->getIri())
+        ));
+
+        return $response;
+    }
+
+        /**
+         * @throws InvalidRequestException
+         * @throws \Apiera\Sdk\Exception\Http\ApiException
+         * @throws \Apiera\Sdk\Exception\Mapping\MappingException
+         */
+    public function create(RequestInterface $request): VariantResponse
+    {
+        if (!$request instanceof VariantRequest) {
+            throw new InvalidRequestException(
+                sprintf('Request must be an instance of %s', VariantRequest::class)
+            );
+        }
+
+        if (!$request->getStore()) {
+            throw new InvalidRequestException('Store IRI is required for this operation');
+        }
+
+        $requestData = $this->mapper->toRequestData($request);
+
+        /** @var VariantResponse $response */
+        $response = $this->mapper->fromResponse($this->client->decodeResponse(
+            $this->client->post($request->getStore() . self::ENDPOINT, $requestData)
+        ));
+
+        return $response;
+    }
+
+        /**
+         * @throws InvalidRequestException
+         * @throws \Apiera\Sdk\Exception\Http\ApiException
+         * @throws \Apiera\Sdk\Exception\Mapping\MappingException
+         */
+    public function update(RequestInterface $request): VariantResponse
+    {
+        if (!$request instanceof VariantRequest) {
+            throw new InvalidRequestException(
+                sprintf('Request must be an instance of %s', VariantRequest::class)
+            );
+        }
+
+        if (!$request->getIri()) {
+            throw new InvalidRequestException('Variant IRI is required for this operation');
+        }
+
+        $requestData = $this->mapper->toRequestData($request);
+
+        /** @var VariantResponse $response */
+        $response = $this->mapper->fromResponse($this->client->decodeResponse(
+            $this->client->patch($request->getIri(), $requestData)
+        ));
+
+        return $response;
+    }
+
+        /**
+         * @throws InvalidRequestException
+         * @throws \Apiera\Sdk\Exception\Http\ApiException
+         */
+    public function delete(RequestInterface $request): void
+    {
+        if (!$request instanceof VariantRequest) {
+            throw new InvalidRequestException(
+                sprintf('Request must be an instance of %s', VariantRequest::class)
+            );
+        }
+
+        if (!$request->getIri()) {
+            throw new InvalidRequestException('Variant IRI is required for this operation');
+        }
+
+        $this->client->delete($request->getIri());
+    }
+}
